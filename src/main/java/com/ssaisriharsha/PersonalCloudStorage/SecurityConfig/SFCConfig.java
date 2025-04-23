f88bcc8c3888aea355f42c51610e4d2a2b51546c
ssaisriharsha <69705177+ssaisriharsha@users.noreply.github.com>
2025-04-24 00:13:44 +0530

    Removed the unnecessary AuthManager class.

--- src/main/java/com/ssaisriharsha/PersonalCloudStorage/SecurityConfig/SFCConfig.java
+++ src/main/java/com/ssaisriharsha/PersonalCloudStorage/SecurityConfig/SFCConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -25,5 +27,9 @@ public class SFCConfig {
                 .formLogin(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable);
         return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
