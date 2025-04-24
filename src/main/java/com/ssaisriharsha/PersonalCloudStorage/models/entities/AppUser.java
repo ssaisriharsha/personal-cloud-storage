5e9fbf7c8466d6b01ab3fefb5dfcf3e89e5531fe
ssaisriharsha <69705177+ssaisriharsha@users.noreply.github.com>
2025-04-24 09:06:17 +0530

    Added unique constraint to email id.

--- src/main/java/com/ssaisriharsha/PersonalCloudStorage/models/entities/AppUser.java
+++ src/main/java/com/ssaisriharsha/PersonalCloudStorage/models/entities/AppUser.java
@@ -1,14 +1,12 @@
 package com.ssaisriharsha.PersonalCloudStorage.models.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.UniqueElements;
 
 import java.time.LocalDateTime;
 
@@ -21,7 +19,7 @@ public class AppUser {
     @Column(name="username")
     private String username;
     @Email
-    @Column(name="email")
+    @Column(name="email", unique = true)
     @NotNull
     private String email;
     @Size(min=10)
